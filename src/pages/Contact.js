2a25de0bdac014e19068d48ab4dda8e57ff5f883
Muhammad Anas <muhammadanas112@gmail.com>
2021-03-25 14:17:20 -0400

    css

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,10 +6,10 @@ function Contact() {
         <div className="contact" style={{ minHeight: '650px', textAlign: 'center'}}>
             <h1 className="jumbotron text-center">Feel free to contact me: </h1>
             <Container>
-                    <li className="lin">Message me on <a href="https://www.linkedin.com/in/muhammad-a-932888153/">LinkedIn</a></li>
+                    <li className="lin">Message me on <a href="https://www.linkedin.com/in/muhammad-a-932888153/">Linkedin</a></li>
                     <li className="git">Find me on <a href="https://github.com/anasm23">GitHub</a></li>
-                  <li className="eeml">Send me an <a href="mailto: muhammadanas112@gmail.com">Email</a></li>
-       </Container> </div>
+                    <li className="eeml">Send me an <a href="mailto: muhammadanas112@gmail.com">Email</a></li>
+            </Container> </div>
         
     );
 }
